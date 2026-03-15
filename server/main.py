59856d3645d75cbd556f926659a09a32f7ab3730
Himanshu Singh <hs7875289@gmail.com>
2026-03-15 12:26:11 +0530

    alembic migrations implemnted

--- server/main.py
+++ server/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from server.config.db import Base, engine
-from server.models import user
 from server.routers import user as user_router
 
 Base.metadata.create_all(bind=engine)
--- server/models/user.py
+++ server/models/user.py
@@ -8,3 +8,7 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+    age = Column(Integer, nullable=True)
+
+    def __repr__(self):
+        return f"<User(name={self.name}, email={self.email}, age={self.age})>"
